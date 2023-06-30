e79be8706e10c56b4687b1fa17842f4d3a3ba07e
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-30 13:56:35 +0300

    Remove excessive using namespace statements

--- src/request_handler.hpp
+++ src/request_handler.hpp
@@ -13,7 +13,6 @@ namespace http = beast::http;
 namespace json = boost::json;
 
 using namespace std::literals;
-using namespace model;
 
 using StringRequest = http::request<http::string_body>;
 // Ответ, тело которого представлено в виде строки
@@ -32,8 +31,6 @@ class RequestHandler {
 
     template <typename Body, typename Allocator, typename Send>
     void operator()(http::request<Body, http::basic_fields<Allocator>> &&request, Send &&send) {
-        using namespace util;
-
         auto json_response = [&request](http::status status, json::value value) {
             return MakeStringResponse(status, json::serialize(value), request.version(), request.keep_alive(),
                                       "application/json"sv);
@@ -46,21 +43,22 @@ class RequestHandler {
             send(json_response(http::status::ok, json::value_from(game_.GetMaps())));
         } else if (target.starts_with(endpoint) && !target.ends_with("/"sv)) {
             std::string_view id = target.substr(endpoint.size() + 1);
-            auto map_id = Map::Id{std::string{id}};
+            auto map_id = model::Map::Id{std::string{id}};
             const auto *map_ptr = game_.FindMap(map_id);
 
             if (map_ptr == nullptr)
                 send(json_response(http::status::not_found,
-                                   json::value_from(Error{"mapNotFound"sv, "Map not found"sv})));
+                                   json::value_from(util::Error{"mapNotFound"sv, "Map not found"sv})));
             else
                 send(json_response(http::status::ok, json::value_from(*map_ptr)));
         } else if (target.starts_with("/api/"sv)) {
-            send(json_response(http::status::bad_request, json::value_from(Error{"badRequest"sv, "Bad request"sv})));
+            send(json_response(http::status::bad_request,
+                               json::value_from(util::Error{"badRequest"sv, "Bad request"sv})));
         }
     }
 
   private:
-    Game &game_;
+    model::Game &game_;
 };
 
 } // namespace request_handler
