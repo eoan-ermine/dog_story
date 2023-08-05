79589fa35c0802ef9f9ac9ca0b43a4abd2dd7dce
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-08-05 10:57:16 +0300

    Move response creation logic from request_handler to response

--- src/request_handler.hpp
+++ src/request_handler.hpp
@@ -25,16 +25,6 @@ using namespace std::literals;
 
 using StringRequest = http::request<http::string_body>;
 
-// Создает text response
-StringResponse MakeTextResponse(http::status status, std::string_view body);
-
-// Создаёт json response
-StringResponse MakeJsonResponse(http::status status, std::string_view body);
-
-// Создает file response
-FileResponse MakeFileResponse(http::status status, std::string_view mime_type, std::string_view filepath,
-                              boost::system::error_code &ec);
-
 class RequestHandler {
   public:
     explicit RequestHandler(model::Game &game, std::string_view static_path) : game_{game}, base_path_(static_path) {}
@@ -59,8 +49,8 @@ class RequestHandler {
             std::string_view id = target.substr(endpoint.size() + 1);
             response = get_map(model::Map::Id{std::string{id}});
         } else if (target.starts_with("/api/"sv)) {
-            response = json_response(http::status::bad_request,
-                                     json::value_from(util::Error{"badRequest"sv, "Bad request"sv}));
+            response = Response::Json(http::status::bad_request,
+                                      json::value_from(util::Error{"badRequest"sv, "Bad request"sv}));
         } else {
             response = get_file(target);
         }
@@ -74,24 +64,20 @@ class RequestHandler {
     }
 
   private:
-    StringResponse json_response(http::status status, json::value value) const {
-        return MakeJsonResponse(status, json::serialize(value));
-    }
-
     // Handle get maps request
     // Endpoint: /api/v1/maps
-    StringResponse get_maps() const { return json_response(http::status::ok, json::value_from(game_.GetMaps())); }
+    Response get_maps() const { return Response::Json(http::status::ok, json::value_from(game_.GetMaps())); }
 
     // Handle get map request
     // Endpoint: /api/v1/maps/{id}
-    StringResponse get_map(model::Map::Id id) const {
+    Response get_map(model::Map::Id id) const {
         const auto *map_ptr = game_.FindMap(id);
 
         if (map_ptr == nullptr)
-            return json_response(http::status::not_found,
-                                 json::value_from(util::Error{"mapNotFound"sv, "Map not found"sv}));
+            return Response::Json(http::status::not_found,
+                                  json::value_from(util::Error{"mapNotFound"sv, "Map not found"sv}));
         else
-            return json_response(http::status::ok, json::value_from(*map_ptr));
+            return Response::Json(http::status::ok, json::value_from(*map_ptr));
     }
 
     // Handle static files requests
@@ -103,11 +89,11 @@ class RequestHandler {
             auto extension = path.extension().string();
 
             boost::system::error_code ec;
-            response = MakeFileResponse(http::status::ok, GetMimeType(extension), path.string(), ec);
+            response = Response::File(http::status::ok, GetMimeType(extension), path.string(), ec);
             if (ec)
-                response = MakeTextResponse(http::status::not_found, "File not found");
+                response = Response::Text(http::status::not_found, "File not found");
         } else {
-            response = MakeTextResponse(http::status::bad_request, "Invalid path");
+            response = Response::Text(http::status::bad_request, "Invalid path");
         }
 
         return response;
--- src/util/response.hpp
+++ src/util/response.hpp
@@ -1,23 +1,62 @@
 #include <boost/beast/http.hpp>
+#include <boost/json.hpp>
 
 #include <type_traits>
 #include <variant>
 
 namespace beast = boost::beast;
 namespace http = beast::http;
+namespace json = boost::json;
 
 using StringResponse = http::response<http::string_body>;
 using FileResponse = http::response<http::file_body>;
 
-template <typename Body>
-void FinalizeResponse(http::response<Body> &response, unsigned http_version, bool keep_alive) {
-    response.version(http_version);
-    response.keep_alive(keep_alive);
-}
-
 class Response {
   public:
     Response() {}
+
+    static Response Text(http::status status, std::string_view body) {
+        StringResponse response;
+        response.result(status);
+        response.set(http::field::content_type, "text/plain");
+        response.body() = body;
+        response.content_length(body.size());
+
+        Response result;
+        result = std::move(response);
+        return result;
+    }
+
+    static Response Json(http::status status, const json::value &value) {
+        StringResponse response;
+        response.result(status);
+        response.set(http::field::content_type, "application/json");
+        response.body() = json::serialize(value);
+        response.content_length(response.body().size());
+
+        Response result;
+        result = std::move(response);
+        return result;
+    }
+
+    static Response File(http::status status, std::string_view mime_type, std::string_view filepath,
+                         boost::system::error_code &ec) {
+        FileResponse response;
+        response.result(status);
+        response.set(http::field::content_type, mime_type);
+
+        http::file_body::value_type file;
+        file.open(filepath.data(), beast::file_mode::read, ec);
+        if (!ec) {
+            response.body() = std::move(file);
+            response.prepare_payload();
+        }
+
+        Response result;
+        result = std::move(response);
+        return result;
+    }
+
     Response &operator=(StringResponse &&response_) {
         response = std::move(response_);
         return *this;
@@ -29,33 +68,35 @@ class Response {
 
     int code() const {
         int code;
-        std::visit([&](auto &&arg) {
-            code = arg.result_int();
-        }, response);
+        std::visit([&](auto &&arg) { code = arg.result_int(); }, response);
         return code;
     }
 
     std::string_view content_type() const {
         std::string_view content_type;
-        std::visit([&](auto &&arg) {
-            content_type = arg.count(http::field::content_type) ? arg[http::field::content_type] : "null";
-        }, response);
+        std::visit(
+            [&](auto &&arg) {
+                content_type = arg.count(http::field::content_type) ? arg[http::field::content_type] : "null";
+            },
+            response);
         return content_type;
     }
 
     void finalize(unsigned http_version, bool keep_alive) {
-        std::visit([&](auto &&arg) {
-            FinalizeResponse(arg, http_version, keep_alive);
-        }, response);
+        std::visit([&](auto &&arg) { FinalizeResponse(arg, http_version, keep_alive); }, response);
     }
 
     template <typename Send>
     void send(Send &&send) {
-        std::visit([&](auto &&arg) {
-            send(arg);
-        }, response);
+        std::visit([&](auto &&arg) { send(arg); }, response);
     }
 
   private:
+    template <typename Body>
+    static void FinalizeResponse(http::response<Body> &response, unsigned http_version, bool keep_alive) {
+        response.version(http_version);
+        response.keep_alive(keep_alive);
+    }
+
     std::variant<StringResponse, FileResponse> response;
 };
