5c14d278e6af870a1cba5235e123a9ae319f9209
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-09-17 11:51:42 +0300

    Isolate changes

--- src/api_handler/endpoints/endpoints.hpp
+++ src/api_handler/endpoints/endpoints.hpp
@@ -2,21 +2,20 @@
 
 #include "fallthrough.hpp"
 #include "game/join.hpp"
-#include "game/player/action.hpp"
-#include "game/player/get_players.hpp"
-#include "game/state/get_state.hpp"
-#include "game/tick.hpp"
+// #include "game/player/action.hpp"
+// #include "game/player/get_players.hpp"
+// #include "game/state/get_state.hpp"
+// #include "game/tick.hpp"
 #include "map/get_map.hpp"
 #include "map/get_maps.hpp"
 #include <memory>
 
 inline std::vector<std::shared_ptr<Endpoint>> GetEndpoints(model::Game &game) {
-    return {std::shared_ptr<Endpoint>{new GetMapEndpoint{game}},
-            std::shared_ptr<Endpoint>{new GetMapsEndpoint{game}},
+    return {std::shared_ptr<Endpoint>{new GetMapEndpoint{game}}, std::shared_ptr<Endpoint>{new GetMapsEndpoint{game}},
             std::shared_ptr<Endpoint>{new JoinEndpoint{game}},
-            std::shared_ptr<Endpoint>{new GetPlayersEndpoint(game)},
-            std::shared_ptr<Endpoint>{new GetStateEndpoint(game)},
-            std::shared_ptr<Endpoint>{new ActionEndpoint{game}},
-            std::shared_ptr<Endpoint>{new TickEndpoint(game)},
+            // std::shared_ptr<Endpoint>{new GetPlayersEndpoint(game)},
+            // std::shared_ptr<Endpoint>{new GetStateEndpoint(game)},
+            // std::shared_ptr<Endpoint>{new ActionEndpoint{game}},
+            // std::shared_ptr<Endpoint>{new TickEndpoint(game)},
             std::shared_ptr<Endpoint>{new FallthroughEndpoint(game)}};
 }
